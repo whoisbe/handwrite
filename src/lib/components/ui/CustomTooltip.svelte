0502f44e09caf731b550353a9106fcf40ece7af9
Bharath Kumar <be@pop-os.tailc3c16.ts.net>
2026-02-04 11:42:42 -0500

    Fix invalid self-closing div in CustomTooltip

--- src/lib/components/ui/CustomTooltip.svelte
+++ src/lib/components/ui/CustomTooltip.svelte
@@ -16,7 +16,7 @@
   {#if isVisible}
     <div class="absolute z-50 px-2 py-1 text-xs text-white bg-gray-900 rounded shadow-sm opacity-100 transition-opacity bottom-full left-1/2 transform -translate-x-1/2 -translate-y-2 whitespace-nowrap pointer-events-none">
       {content}
-      <div class="absolute top-full left-1/2 transform -translate-x-1/2 -mt-1 border-4 border-transparent border-t-gray-900" />
+      <div class="absolute top-full left-1/2 transform -translate-x-1/2 -mt-1 border-4 border-transparent border-t-gray-900"></div>
     </div>
   {/if}
 </div>
